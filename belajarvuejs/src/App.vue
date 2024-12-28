7a0808ebda3ba05251b6c88f9b42fd0b5263acba
Alwiii <210209501011@student.unm.ac.id>
2024-12-28 12:48:45 +0800

    custom event

--- belajarvuejs/src/App.vue
+++ belajarvuejs/src/App.vue
@@ -2,7 +2,15 @@
   <div>
     <appHeader></appHeader>
     <div class="container">
-      <profilPage :name="name" :lastname="lastname"></profilPage>
+      <profilPage
+        :aka="name"
+        :lastname="lastname"
+        :userAge="age"
+        :userParents="parents"
+        @update-lastname="lastname = $event"
+        @say-hello="sayHello"
+        :updateAge="updateAge"
+      ></profilPage>
     </div>
     <button @click="changeName">Ubah Name</button>
     <appFooter></appFooter>
@@ -22,11 +30,22 @@ export default {
     return {
       name: 'Muhammad',
       lastname: 'Alwi',
+      age: 21,
+      parents: {
+        father: 'Budi',
+        mother: 'Siti',
+      },
     }
   },
   methods: {
     changeName() {
       this.name = 'wial'
+    },
+    sayHello() {
+      alert('Hello Alwi')
+    },
+    updateAge(value) {
+      this.age = value
     },
   },
 }
--- belajarvuejs/src/components/user/profilPage.vue
+++ belajarvuejs/src/components/user/profilPage.vue
@@ -2,13 +2,40 @@
   <div class="userProfile">
     <h3>user information</h3>
     <ul>
-      <li><span>First Name :</span>{{ name }}</li>
-      <li><span>Last Name :</span>{{ lastname }}</li>
+      <li><span>Ask Now As : </span>{{ aka }}</li>
+      <li><span>Name : </span>{{ name }}</li>
+      <li><span>Last Name : </span>{{ lastname }}</li>
+      <li><span>Age : </span>{{ userAge }}</li>
+      <ul>
+        <li v-for="(value, key, index) in userParents" :key="index">{{ key }} : {{ value }}</li>
+      </ul>
     </ul>
+    <button @click="updateLastName">UpdateLastName</button>
+    <button @click="sayHello">Klik hello</button>
+    <button @click="updateAge(22)">update age</button>
   </div>
 </template>
 <script>
 export default {
-  props: ['name', 'lastname'],
+  props: {
+    aka: String,
+    lastname: String,
+    userAge: Number,
+    userParents: Object,
+    updateAge: Function,
+  },
+  data() {
+    return {
+      name: 'Alwi',
+    }
+  },
+  methods: {
+    updateLastName() {
+      this.$emit('update-lastname', 'alfiah')
+    },
+    sayHello() {
+      this.$emit('say-hello')
+    },
+  },
 }
 </script>
