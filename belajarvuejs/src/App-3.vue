9afd07abde4bd8187ed7adae753325a4919e88b7
Alwiii <210209501011@student.unm.ac.id>
2025-01-05 20:52:20 +0800

    lifecycle

---
+++ belajarvuejs/src/App-3.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <appHeader></appHeader>
+    <div class="container">
+      <compLife></compLife>
+    </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import appHeader from './components/generals/appHeader.vue'
+import appFooter from './components/generals/appFooter.vue'
+import compLife from './components/life/index-life.vue'
+export default {
+  components: {
+    appHeader,
+    appFooter,
+    compLife,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ belajarvuejs/src/components/life/index-life.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>lifeCycle-hook</div>
+</template>
